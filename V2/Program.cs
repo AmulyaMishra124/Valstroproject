ac48c3993705c4fdababcfdbeb75a9fdc3c86a5d
Sarita Mishra <snmishra@ets.org>
2022-05-15 23:45:09 -0400

    Commit to public repo

---
+++ V2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the character Name:");
+            string searchName = Console.ReadLine().ToLower();
+            while (true) { 
+                if (string.IsNullOrWhiteSpace(searchName))
+                {
+                    Console.WriteLine("Enter a valid string.");
+
+                }
+                else
+                {
+                    SearchPeople(searchName);
+                    return;
+                }
+            }
+            
+
+        }
+
+        public static void SearchPeople(string searchName)
+        {
+            try
+            {
+
+            
+            using (var client = new HttpClient())
+            {
+                var endPoint = new Uri("https://www.swapi.tech/api/people");
+                var result = client.GetAsync(endPoint).Result.Content.ReadAsStringAsync().Result;
+                var peopleColl = JsonConvert.DeserializeObject<Starwars>(result);
+                    bool found = false;
+                Console.WriteLine("Search Details:");
+
+                foreach (people item in peopleColl.results)
+                {
+                    if (item.name.ToLower().Contains(searchName.ToLower()))
+                    {
+                            Console.WriteLine(item.name);
+                            found = true;
+                    }
+                     
+                 }
+                    if (!found) { Console.WriteLine("No results found."); }
+                Console.ReadLine();
+               }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
---
+++ V2/StarwarsPeople.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace V2
+{
+
+    public class Starwars
+    {
+
+        public string message { get; set; }
+        public int total_records { get; set; }
+        public int total_pages { get; set; }
+        public string next { get; set; }
+
+        public List<people> results { get; set; }
+
+
+    }
+    public class people
+    {
+        public string uid { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+
+
+    }
+}
